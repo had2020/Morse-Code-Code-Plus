4baad77dff7aa59f7785ec2f472ec67113d2b8b7
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-10-07 21:32:19 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
-    if let Some(second) = env::args().nth(1) {
-        println!("arg: {}", second);
+    if let Some(filepath) = env::args().nth(1) {
+        let file = File::open(filepath).unwrap();
+        let reader = io::BufReader::new(file);
+
+        let mut line_buffer: Vec<String> = vec![];
+
+        for line in reader.lines() {
+            line_buffer.push(line.unwrap());
+        }
     } else {
         println!("Please include filepath, as command line argument!");
         std::process::exit(1);
